958d3b7e4f7d4b0ce30f5daf881da3c72b405940
trudk <trudkhanov@inbox.ru>
2024-05-09 21:51:03 +0500

    created class Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Random;
+
+class MyTestingClass {
+    private int id;
+
+    public MyTestingClass(int id) {
+        this.id = id;
+    }
+
+    // Custom hashCode method
+    @Override
+    public int hashCode() {
+        // Custom hash function for better distribution
+        return id * 31;
+    }
+
+    // Implement equals method for completeness
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        MyTestingClass other = (MyTestingClass) obj;
+        return id == other.id;
     }
 }
+
+//public class Main {
+//    public static void main(String[] args) {
+//        MyHashTable<MyTestingClass, String> table = new MyHashTable<>();
+//        Random random = new Random();
+//
+//
+//        for (int i = 0; i < 10000; i++) {
+//            MyTestingClass key = new MyTestingClass(random.nextInt(100));
+//            table.put(key, "Value " + i);
+//        }
+//
+//
+//        for (int i = 0; i < table.getCapacity(); i++) {
+//            System.out.println("Bucket " + i + ": " + table.get());
+//        }
+//    }
+//}
--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,38 +1,115 @@
+import java.util.Objects;
+
 public class MyHashTable<K, V> {
-    private class HashNode<K, V>{
+    private class HashNode<K, V> {
         private K key;
         private V value;
         private HashNode<K, V> next;
 
-        public HashNode(K key , V value){
+        public HashNode(K key, V value) {
             this.key = key;
             this.value = value;
         }
 
         @Override
-        public String toString(){
-            return "{"+ key +" "+ value+"}";
+        public String toString() {
+            return "{" + key + " " + value + "}";
         }
     }
 
 
     private HashNode<K, V>[] chainArray;
-    private int M=11;
+    private int M = 11;
     private int size;
+    private int capacity;
 
-    public MyHashTable() {}
+    public MyHashTable() {
+        this.capacity = M;
+        this.chainArray = (HashNode<K, V>[]) new HashNode[capacity];
+    }
 
-    public MyHashTable(int M){}
 
-//    private int hash(K key){}
+    public MyHashTable(int capacity) {
+        this.capacity = capacity;
+        this.chainArray = (HashNode<K, V>[]) new HashNode[capacity];
+    }
 
-    public void put(K key, V value){}
+    private int hash(K key) {
+        return Math.abs(key.hashCode() % capacity);
+    }
 
-//    public V get(K key){}
-//    public V remove(K key){}
+    public void put(K key, V value) {
+        int index = hash(key);
+        HashNode<K, V> newNode = new HashNode<>(key, value);
+        if (chainArray[index] == null) {
+            chainArray[index] = newNode;
+        } else {
+            HashNode<K, V> current = chainArray[index];
+            while (current.next != null) {
+                if (current.key.equals(key)) {
+                    current.value = value;
+                    return;
+                }
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
 
-//    public boolean contains(V value){}
+    public V get(K key) {
+        int index = hash(key);
+        HashNode<K, V> current = chainArray[index];
+        while (current != null) {
+            if (current.key.equals(key)) {
+                return current.value;
+            }
+            current = current.next;
+        }
+        return null;
+    }
 
-//    public K getKey(V value){}
+    public V remove(K key) {
+        int index = hash(key);
+        HashNode<K, V> current = chainArray[index];
+        HashNode<K, V> prev = null;
+        while (current != null) {
+            if (current.key.equals(key)) {
+                if (prev == null) {
+                    chainArray[index] = current.next;
+                } else {
+                    prev.next = current.next;
+                }
+                size--;
+                return current.value;
+            }
+            prev = current;
+            current = current.next;
+        }
+        return null;
+    }
 
+    public boolean contains(V value) {
+        for (HashNode<K, V> node : chainArray) {
+            while (node != null) {
+                if (Objects.equals(node.value, value)) {
+                    return true;
+                }
+                node = node.next;
+            }
+        }
+        return false;
+    }
+
+    public K getKey(V value) {
+        for (HashNode<K, V> node : chainArray) {
+            while (node != null) {
+                if (Objects.equals(node.value, value)) {
+                    return node.key;
+                }
+                node = node.next;
+            }
+        }
+        return null;
+    }
 }
